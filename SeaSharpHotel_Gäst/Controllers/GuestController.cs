38bd1a495a04b9558cf0f08f6e80b8ecfb769ee2
Altug Yildiz <alyi0001@student.hv.se>
2021-02-16 18:36:34 +0100

    Jag har gjort lite. skapa Guest controller men kan inte läägga in url eftersom vi inte har published än.

---
+++ SeaSharpHotel_Gäst/Controllers/GuestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeaSharpHotel_Gäst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SeaSharpHotel_Gäst.Controllers
+{
+    public class GuestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            //skapar ny guest
+            List<Guest> guests = new List<Guest>();
+            //http call out backend
+            HttpClient client = new HttpClient();
+            //for respons vi måste publish innan jag kan lägga in url annars blir et bara localhostadress
+            var respons = await client.GetAsync("");
+            return View();
+        }
+    }
+}
